06c90f0988774bb1c5385b661753feb99d4c1182
pincha100 <113956174+pincha100@users.noreply.github.com>
2024-10-15 22:23:25 +0700

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -1,11 +1,9 @@
 import time
 from multiprocessing import Pool
 
-
-# Функция для чтения информации из файла
 def read_info(name):
     all_data = []
-    with open(name, 'r') as file:
+    with open(name, 'r', encoding='utf-8') as file:
         while True:
             line = file.readline()
             if not line:
@@ -13,22 +11,18 @@ def read_info(name):
             all_data.append(line)
     return all_data
 
-
-# Основная программа
 if __name__ == '__main__':
-    # Список файлов
-    filenames = [f'./file 4.txt' for number in range(1, 5)]
+    # Список названий файлов с пробелом в имени
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
-    # Линейный вызов
+    # Линейное чтение файлов
     start_time = time.time()
     for filename in filenames:
         read_info(filename)
-    end_time = time.time()
-    print(f"Линейный вызов занял: {end_time - start_time:.6f} секунд")
+    print(f"Линейный вызов: {time.time() - start_time} секунд")
 
-    # Многопроцессный вызов
+    # Многопроцессорное чтение файлов
     start_time = time.time()
     with Pool() as pool:
         pool.map(read_info, filenames)
-    end_time = time.time()
-    print(f"Многопроцессный вызов занял: {end_time - start_time:.6f} секунд")
+    print(f"Многопроцессорный вызов: {time.time() - start_time} секунд")
